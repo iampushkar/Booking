7cc9fb506887a0413973d0bfa3025870dfe28b2a
Manish Pushkar <m4manishpushkar@gmail.com>
2022-07-26 08:50:24 +0530

    Initial Springboot app

---
+++ src/main/java/com/booking/hotel/HotelApplication.java
@@ -0,0 +1,14 @@
+package com.booking.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+
+	public static void main(String[] args) {
+		System.out.println("--------------Manish here---------------");
+		SpringApplication.run(HotelApplication.class, args);
+	}
+
+}
